1a601a0c90b72e132e10fcae8835b9b3ee036ac1
Mikhail Stupnikov <Mikhail.stupnikov@gmail.com>
2016-05-29 12:57:51 +0300

    DOM

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+/*
+1) Find a jQuery plugin for creating a photo gallery or write your own script.
+
+2) The gallery must include the ability to click on photos and view them in a lightbox (see the photo_lightbox.png file for the design).
+3) Add text captions to the images when viewed in the lightbox.
+-See the photo_lightbox.png file for the design.
+4) Add back and forward buttons when the lightbox is visible to switch between photos.
+5) Implement the search box at the top of the page that filters photos based on the captions.
+6) The photos should filter in real-time as you type. - keypress & keyup
+7) This could be a jQuery plugin that you find on the web, or code that you write yourself.*/
